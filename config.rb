bfa57600066a222d674857f0e1efe6e89625035b
Johannes Klumpe <klumpe@live.de>
2018-01-18 12:00:40 +0100

    new urls

--- config.rb
+++ config.rb
@@ -40,6 +40,11 @@ page '/*.txt', layout: false
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
+
+configure :build do
+  activate :relative_assets
+end
+
 # configure :build do
 #   activate :minify_css
 #   activate :minify_javascript
