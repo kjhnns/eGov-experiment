a343555eef39ce3ff2a851eb21a0d92c0b694a37
Johannes Klumpe <klumpe@live.de>
2018-02-06 12:59:09 +0100

    added dopt and validation

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,8 @@
 // This is where it all goes :)
+function validateForm() {
+    var x = document.forms["sosci"]["eid"].value;
+    if (x == "") {
+        alert("Please select ID type");
+        return false;
+    }
+}
